e819a94af8f05aa4957ead26ebcaacf57be10904
Gauthier Voron <gauthier.voron@epfl.ch>
2023-09-14 17:11:01 +0200

    remove aws semaphore

--- src/aws/reach.mk
+++ src/aws/reach.mk
@@ -1,7 +1,7 @@
 $(call GENERATE-DIR, $(RUN)reach)
 
-$(RUN)reach/aws.%: $(RUN)start/aws.% | $(AWS-SEMAPHORE) $(RUN)reach
+$(RUN)reach/aws.%: $(RUN)start/aws.% | $(RUN)reach
 	$(call cmd-print,  REACH   $@)
-	$(Q)$(AWS-SRC)reach $@ $(AWS-SEMAPHORE) $<
+	$(Q)$(AWS-SRC)reach $@ $<
 
 PHONY-COMMANDS += reach
