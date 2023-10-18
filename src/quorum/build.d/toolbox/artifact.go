c282d39144038bd4087a992dc342ba2166efac4d
Gauthier Voron <gauthier.voron@epfl.ch>
2023-10-18 16:55:15 +0200

    copy remote scripts to build.d

---
+++ src/quorum/build.d/toolbox/artifact.go
@@ -0,0 +1,25 @@
+package main
+
+// import (
+
+// )
+
+
+// // ----------------------------------------------------------------------------
+
+
+// type Artifact struct {
+// 	path string
+// }
+
+
+// func NewArtifact(path string) (*Artifact, error) {
+
+// }
+
+// func (this *Artifact) IntoNode(path string) error {
+
+// }
+
+
+// // ----------------------------------------------------------------------------
