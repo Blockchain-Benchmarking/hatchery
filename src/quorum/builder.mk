38e8176dd5b5c9f262aa03db87f41ee24e39870a
Gauthier Voron <gauthier.voron@epfl.ch>
2023-10-18 17:00:07 +0200

    remove dead code

--- src/quorum/builder.mk
+++
@@ -1,9 +0,0 @@
-$(call GENERATE-DIR, $(RUN)quorum-builder)
-
-$(RUN)quorum-builder/%: $(RUN)silk/% | $(RUN)quorum-builder
-	$(call cmd-print,  QUORUM  $@)
-	$(call cmd-run, $(QUORUM-SRC)builder $@ $<)
-
-PHONY-COMMANDS += quorum-builder
-
-.NOTINTERMEDIATE: $(RUN)quorum-builder/%
--- src/quorum/network.mk
+++
@@ -1,8 +0,0 @@
-$(call GENERATE-DIR, $(ASSET)quorum)
-
-$(ASSET)quorum/network.%: $(quorum-builder) | $(ASSET)quorum
-	$(call cmd-print,  QUORUM  $@)
-	$(call cmd-run, $(QUORUM-SRC)network $@ \
-                        $(patsubst $(ASSET)quorum/network.%, %, $@) $<)
-
-.NOTINTERMEDIATE: $(ASSET)quorum/network.%
