56834d6964f20a7386b2b81f04c344dde8001962
Gauthier Voron <gauthier.voron@epfl.ch>
2023-11-01 16:17:57 +0100

    add a rule to build the quorum toolbox locally

--- src/quorum/mod.mk
+++ src/quorum/mod.mk
@@ -8,6 +8,38 @@ system := quorum
 
 
 module-path := $(dir $(lastword $(MAKEFILE_LIST)))
+module-cache-path := $(module-path).cache/
 
-include $(module-path)build.mk
-include $(module-path)prepare.mk
+
+
+
+# Some scripts need a compiled version of the toolbox on the controller
+# machine. These rules make the scripts depend on the compiled toolbox for this
+# reason.
+#
+
+
+toolbox-deps := $(call REGULARS, $(call FIND, $(module-path)build.d/toolbox))
+
+$(call REQUIRE-DIR, $(module-cache-path)toolbox)
+
+$(module-cache-path)toolbox: $(module-path)build-toolbox $(toolbox-deps)
+	$(call cmd-print,  BUILD   $@)
+	$(call cmd-run, $<)
+
+
+.PHONY: stop
+stop: clean/$(system)/.cache
+
+.PHONY: clean/$(system)/.cache
+
+define template
+
+  clean/$(system)/.cache:
+	$$(call cmd-clean, $(module-cache-path))
+
+endef
+
+$(eval $(call template))
+
+undefine template
